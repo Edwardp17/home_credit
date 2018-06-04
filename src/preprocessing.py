b6a6d98740fab4b2f4bd0e1b8ecb30c82fa49daf
Edgar Pavlovsky <edgarpavlovsky@gmail.com>
2018-06-03 20:37:42 -0700

    finishing v1 application_train feature engineering

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,6 +5,7 @@ gc.enable()
 import random
 import numpy as np 
 import pandas as pd
+from scipy import stats
 
 DATA_PATH = '../input/'
 DATASET_FILENAMES = []
@@ -55,9 +56,13 @@ class Dataset:
     def transform_binary_features(self):
 
         # redundant statements like this can be put in a decorator
-        if self.data = None:
+        if self.data == None:
 
             raise Exception('Data cannot be None for binary vars to be discovered.')
+
+        if self.binary_vars == None:
+
+                raise Warning('Tried to transform binary vars but found no binary vars. Making no transformations.')
 
         for col in self.binary_vars:
 
@@ -109,29 +114,63 @@ class Preprocessor:
 
             raise Exception('Could not find application_train in Preprocessor.datasets')
 
-        # set target variable and join_key
-        # df_application_train.target = df_application_train.data['TARGET']
-        df_application_train.join_key = ['SK_ID_CURR']
+        
+        # ================
+        # Target variable, join key, exclude vars
+        # ================
 
-        # set exclude vars
+        df_application_train.target = df_application_train.data['TARGET']
+        df_application_train.join_key = ['SK_ID_CURR']
         df_application_train.exclude_vars = ['TARGET']
 
-        df_application_train.data['PERC_FAM_MEMBERS_CHILDREN'] = (1.0 * df_application_train.data.CNT_CHILDREN) / df_application_train.data.CNT_FAM_MEMBERS
+        # ================
+        # Cleaning
+        # ================
 
         df_application_train.data.WEEKDAY_APPR_PROCESS_START = df_application_train.data.WEEKDAY_APPR_PROCESS_START.apply(lambda x: str(x))
         df_application_train.data.HOUR_APPR_PROCESS_START = df_application_train.data.HOUR_APPR_PROCESS_START.apply(lambda x: str(x))
 
+        # ================
+        # Feature engineering
+        # ================
+
+        # returns a modified dataframe that has num var percentiles partitioned by each cat var
+        def num_percentiles_partitioned_by_cat_var(df):
+
+            num_vars = []
+            cat_vars = []
+            for col in df.columns:
+
+                if type(df[col][0]) in (int, float):
+
+                    num_vars.append(col)
+                
+                elif type(df[col][0]) in (str):
+
+                    cat_vars.append(col)
+
+            for cat_var in cat_vars:
+                for num_var in num_vars:
+
+                    df[str(cat_var) + '_' + str(num_var) + '_rankpartition'] = df.groupby(cat_var)[num_var].rank()
+            
+            return df
+
         df_application_train.data['ORGANIZATION_TYPE_GROUP'] = df_application_train.data.ORGANIZATION_TYPE.apply(lambda x: x.split(':')[0].split('Type')[0])
+        df_application_train.data['PERC_FAM_MEMBERS_CHILDREN'] = (1.0 * df_application_train.data.CNT_CHILDREN) / df_application_train.data.CNT_FAM_MEMBERS
+        
+        # get num var percentiles partitioned by each cat var
+        df_application_train.data = num_percentiles_partitioned_by_cat_var(df_application_train.data)
 
         # ================
-        # binary vars
+        # Standardize binary vars
         # ================
 
         df_application_train.discover_binary_vars()
         df_application_train.transform_binary_features()
 
         # ================
-        # concatenating everything
+        # Get final feature set
         # ================
 
         self.df_application_train_features = df_application_train.data[[x for x in df_application_train.data.columns if x not in df_application_train.exclude_vars]]
